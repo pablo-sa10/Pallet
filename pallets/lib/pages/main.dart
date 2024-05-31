2feda49cff5ebbf9d38bbdcaebd7b6b65e5946c1
Pablo Roberto <pablomoura2016@gmail.com>
2024-05-31 16:40:46 -0300

    consegui salvar o modo q o usuario escolheu mesmo depois de reiniciar o app

--- pallets/lib/pages/main.dart
+++ pallets/lib/pages/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:pallets/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'Home.dart';
 import 'cadastro.dart';
 
 Future<void> main() async {
-  runApp(ChangeNotifierProvider(
-      create: (context) => ThemeProvider(), child: const MyApp()));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isDark = prefs.getBool('isDarkTheme') ?? false;
+
+  ThemeProvider themeProvider = ThemeProvider(isDark);
+  runApp(
+    ChangeNotifierProvider.value(
+      value: themeProvider,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,19 +26,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Pallets',
-          theme: themeProvider.isDarkTheme ? ThemeData.dark() : ThemeData.light(),
-          initialRoute: '/',
-          routes: {
-            '/': (context) => const Home(),
-            "/Cadastro": (context) => const Cadastro(),
-          },
-        );
-      }
-    );
+    return Consumer<ThemeProvider>(builder: (context, themeProvider, child) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Pallets',
+        theme: themeProvider.isDarkTheme ? ThemeData.dark() : ThemeData.light(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const Home(),
+          "/Cadastro": (context) => const Cadastro(),
+        },
+      );
+    });
   }
 }
--- pallets/lib/theme/theme_provider.dart
+++ pallets/lib/theme/theme_provider.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier{
   bool _isDark = false;
+
+  ThemeProvider(bool isDark){
+    _loadTheme();
+  }
 
   bool get isDarkTheme => _isDark;
 
   void toggleTheme(){
     _isDark = !_isDark;
+    _saveTheme();
     notifyListeners();
+  }
+
+  void _loadTheme()async{
+    final prefs = await SharedPreferences.getInstance();
+    _isDark = prefs.getBool('isDarkTheme') ?? false;
+    notifyListeners();
+  }
+
+  void _saveTheme()async{
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkTheme', _isDark);
   }
 }
